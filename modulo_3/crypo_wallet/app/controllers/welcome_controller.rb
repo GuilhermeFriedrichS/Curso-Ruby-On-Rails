ae959d82f1fedf671ce73cc703f6e41f8b53d7dd
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-12-16 17:09:30 -0300

    Changed variables to get params from URL

--- modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
+++ modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @nome = "Jackson"
-    @curso = "Rails"
+    @meu_nome = params[:nome] #Pega o parametro que vem pela URL (Query Params)
+    @meu_curso = params[:curso]
   end
 end
