c58ee85e814b2d836c362a06ee2d74765da66ed3
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-12-16 14:56:15 -0300

    Configuring default routes

--- modulo_3/crypo_wallet/config/routes.rb
+++ modulo_3/crypo_wallet/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  resources :coins
+  get 'welcome/index'#, to: 'welcome#index' < Poderia ser assim também, mas ele suprimiu pois os nomes são iguais
+  #get '/inicio', to: 'welcome#index'
+ 
+  resources :coins #Gera as 7 rotas de CRUD pra tabela Coins
+  #get '/coins', to: 'coins#index'
+
+  root to: 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
