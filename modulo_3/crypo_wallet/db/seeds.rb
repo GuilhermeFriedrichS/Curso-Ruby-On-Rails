81b0f045db543bae67b1492481c719e8222ae6d2
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-01-02 09:33:30 -0300

    Configuring db:seed task

--- modulo_3/crypo_wallet/db/seeds.rb
+++ modulo_3/crypo_wallet/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando moedas..."
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/Logo-Bitcoin-PNG.png"
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETC",
+    url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1257px-Ethereum_logo_2014.svg.png"
+)
+
+Coin.create!(
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
+)   
+
+puts "Moedas criadas com sucesso!..."
