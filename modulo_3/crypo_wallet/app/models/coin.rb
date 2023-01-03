4940dbb6511d4a712bc99e999aaa8c08cd128e1e
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-01-03 09:18:52 -0300

    Created associations in models

--- modulo_3/crypo_wallet/app/models/coin.rb
+++ modulo_3/crypo_wallet/app/models/coin.rb
@@ -1,2 +1,3 @@
 class Coin < ApplicationRecord
+    belongs_to :mining_type #Faz a associação do id dos mining type
 end
--- modulo_3/crypo_wallet/app/models/mining_type.rb
+++ modulo_3/crypo_wallet/app/models/mining_type.rb
@@ -1,2 +1,3 @@
 class MiningType < ApplicationRecord
+    has_many :coins #Permite ver do MiningType as moedas associadas
 end
