c248741ed2c989ef6ce9613f510133b0747a9d00
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-01-04 08:33:02 -0300

    Creating and using variables of cookies and sessions

--- modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
+++ modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails [SESSION]"
     @meu_nome = params[:nome] #Pega o parametro que vem pela URL (Query Params)
     @meu_curso = params[:curso]
   end
