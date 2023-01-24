ab4ce962aae65b15ec467eefa5d75d15c7716c81
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-01-24 10:28:26 -0300

    Site layout config

--- modulo_4/time_to_answer/config/routes.rb
+++ modulo_4/time_to_answer/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -8,8 +11,8 @@ Rails.application.routes.draw do
   devise_for :profiles
   devise_for :admins
   get 'welcome/index'
-  get 'inicio', to: 'welcome#index'
+  get 'inicio', to: 'site/welcome#index'
 
-  root to: 'welcome#index' #Altera a rota padrão
+  root to: 'site/welcome#index' #Altera a rota padrão
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
