9af88a86ac78af4d51b348a8af23bcb39ed33333
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2023-01-30 11:03:45 -0300

    Create Subjects controller

--- modulo_4/time_to_answer/config/routes.rb
+++ modulo_4/time_to_answer/config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' #Dashboard
-    resources :admins #Admins
+    resources :admins   #Admins
+    resources :subjects # Assuntos/Áreas
   end
   
   devise_for :admins
